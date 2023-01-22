00d7abced61b0290f817b15bc87fa6b307d1fd34
Jakinlade <jj.47.johnson@gmail.com>
2023-01-22 02:47:50 +0000

    updated

--- assets/style.js
+++ assets/style.js
@@ -1,22 +1,41 @@
-// This JavaScript file is for handling form submissions, validation, and any other interactivity on the website
+$(document).ready(function() {
+    // Hide the "attending options" and "volunteer options" sections initially
+    $("#attending-options, #volunteer-question").hide();
+    $("#sorry-message").hide();
+    $("#volunteer-options").hide();
+    $("#success-message").hide();
 
-// Select the form element
-const form = document.querySelector('form');
+    // Show the "attending options" section when user selects "yes" for attending
+    $("#attending-yes").click(function() {
+        $("#attending-options").show();
+        $("#continue-button").show();
+    });
+    // Show the "sorry" message when user selects "no" for attending
+    $("#attending-no").click(function() {
+        $("#sorry-message").show();
+    });
 
-// Add a submit event listener to the form
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
+    // Show the "volunteer question" section when the user clicks the "continue" button
+    $("#continue-button").click(function() {
+        $("#volunteer-question").show();
+    });
 
-    // Get the values of the form fields
-    const name = document.querySelector('#name').value;
-    const email = document.querySelector('#email').value;
-    const days = document.querySelectorAll('input[name="days"]:checked');
-    const volunteer = document.querySelector('#volunteer').checked;
+    // Show the "volunteer options" section when user selects "yes" for volunteering
+    $("#volunteer").click(function() {
+        $("#volunteer-options").show();
+    });
 
-    // Send an email to JJ with the RSVP information
-    sendEmail(name, email, days, volunteer);
-
-    // Display a message to the user
-    alert(`Thank you for your RSVP, ${name}! You have selected to attend on ${getDaysAttending(days)} and ${volunteer ? 'have offered to help with cooking and cleaning.' : 'have not offered to help with cooking and cleaning.'}`);
+    // Send form data to email address when submit button is clicked
+    $("#rsvp-form").submit(function(e) {
+        e.preventDefault();
+        var formData = $(this).serialize();
+        $.ajax({
+            type: "POST",
+            url: "https://example.com/send-form-data.php",
+            data: formData + "&email=jj.47.johnson@gmail.com",
+            success: function() {
+                $("#success-message").show();
+            }
+        });
+    });
 });
-
